694ef48b10d442269d272fe7f6e47a8559e6c7f2
franciscomaik <chiconunes3081@gmail.com>
2019-08-29 23:16:15 -0300

    Classe de Produtos modificado com teste

--- produtos.py
+++ produtos.py
@@ -6,7 +6,7 @@ class Produto:
         self._identificador = identificador
         self._preco_unitario = preco_unitario
 
-    #+++++++++++++++++++++++nome==================
+    #____________________________nome____________________________
     @property
     def nome_do_produto(self):
         return self._nome_do_produto
@@ -15,8 +15,7 @@ class Produto:
     def nome_do_produto(self,novo_nome):
         self._nome_do_produto = novo_nome
         return True
-    #+++++++++++++++++++++quantidade+++++++++++++
-
+    #____________________________quantidade____________________________
     @property
     def quantidade(self):
         return self._quantidade
@@ -32,10 +31,12 @@ class Produto:
         self._quantidade += valor
         return True
 
+    # ____________________________identificador____________________________
     @property
     def identificador(self):
         return self._identificador
 
+    # ____________________________preço____________________________
     @property
     def preco_unitario(self):
         return self._preco_unitario
@@ -43,15 +44,15 @@ class Produto:
     def alterar_valor(self,valor):
         self._preco_unitario = valor
 
-
+#____________________________teste de classe____________________________
 prod = Produto('Cimento',1,43.50,42)
-prod.quantidade
-prod.nome_do_produto
-prod.preco_unitario
-prod.identificador
+print('Quantidade do produto:',prod.quantidade)
+print('Nome do produto:',prod.nome_do_produto)
+print('Preço do produto:',prod.preco_unitario)
+print('Identificador do produto:',prod.identificador)
 
 prod.adicionar_quantidade(10)
-prod.quantidade
+print('Quantidade do produto:',prod.quantidade)
 prod.retirar_quantidade(3)
-prod.quantidade
+print('Quantidade do produto:',prod.quantidade)
 
